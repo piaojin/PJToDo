1a0e1093185e5b86ea4b2ae08f4dc1dbd7dba655
Zoey Weng <zoey.weng@ringcentral.com>
2018-10-05 16:45:44 +0800

    Add the Swift bridge file to Rust Core Lib

---
+++ PJToDo/iOS/PJToDo/PJToDoCoreLib/Delegates/ToDo/ToDoDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoDelegate.swift
+//  PJToDo
+//
+//  Created by Zoey Weng on 2018/10/5.
+//  Copyright © 2018年 piaojin. All rights reserved.
+//
+
+import Foundation
+
+protocol ToDoDelegate {
+    
+}
