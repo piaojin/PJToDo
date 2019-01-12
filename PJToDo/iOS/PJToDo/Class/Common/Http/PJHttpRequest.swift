69555cd07f0c04c5ef93da12e3f5d8df7ad5f5f1
piaojin <13666902838@163.com>
2019-01-12 17:54:22 +0800

    Finish adding Rust http request api to Swift.

--- PJToDo/iOS/PJToDo/Class/Common/Http/PJHttpRequest.swift
+++ PJToDo/iOS/PJToDo/Class/Common/Http/PJHttpRequest.swift
@@ -10,6 +10,9 @@ import Foundation
 import CocoaLumberjack
 
 public struct PJHttpRequest {
+    
+    //MARK: User
+    
     public static func login(name: String, passWord: String, responseBlock: ((_ data: User?, _ isSuccess : Bool) -> Void)?) {
         let httpRequestConfig = PJHttpRequestConfig(responseBlock: { (pointer, isSuccess) -> Void in
             DispatchQueue.main.async(execute: {
@@ -87,5 +90,53 @@ public struct PJHttpRequest {
         })
         
         PJ_RequestUserInfo(httpRequestConfig.iDelegate)
+    }
+    
+    //MARK: Repos
+    
+    public static func createGitHubRepos(responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) {
+        let httpRequestConfig = self.createHttpRequestConfig(responseBlock: responseBlock)
+        PJ_CreateRepos(httpRequestConfig.iDelegate)
+    }
+    
+    public static func getGitHubRepos(reposUrl: String, responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) {
+        let httpRequestConfig = self.createHttpRequestConfig(responseBlock: responseBlock)
+        PJ_GetRepos(httpRequestConfig.iDelegate, reposUrl)
+    }
+    
+    public static func deleteGitHubRepos(reposUrl: String, responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) {
+        let httpRequestConfig = self.createHttpRequestConfig(responseBlock: responseBlock)
+        PJ_DeleteRepos(httpRequestConfig.iDelegate, reposUrl)
+    }
+    
+    //MARK: File
+    
+    public static func createGitHubFile(path: String, message: String, content: String, sha: String, responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) {
+        let httpRequestConfig = self.createHttpRequestConfig(responseBlock: responseBlock)
+        PJ_CreateFile(httpRequestConfig.iDelegate, path, message, content, sha)
+    }
+    
+    public static func updateGitHubFile(path: String, message: String, content: String, sha: String, responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) {
+        let httpRequestConfig = self.createHttpRequestConfig(responseBlock: responseBlock)
+        PJ_UpdateFile(httpRequestConfig.iDelegate, path, message, content, sha)
+    }
+    
+    public static func deleteGitHubFile(path: String, message: String, content: String, sha: String, responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) {
+        let httpRequestConfig = self.createHttpRequestConfig(responseBlock: responseBlock)
+        PJ_DeleteFile(httpRequestConfig.iDelegate, path, message, content, sha)
+    }
+    
+    private static func createHttpRequestConfig(responseBlock: ((_ resultString: String?, _ isSuccess : Bool) -> Void)?) -> PJHttpRequestConfig {
+        let httpRequestConfig = PJHttpRequestConfig(responseBlock: { (pointer, isSuccess) -> Void in
+            DispatchQueue.main.async(execute: {
+                if let tempPointer = pointer {
+                    let jsonString = String.create(cString: tempPointer)
+                    responseBlock?(jsonString, isSuccess)
+                } else {
+                    responseBlock?(nil, isSuccess)
+                }
+            })
+        })
+        return httpRequestConfig
     }
 }
